18452968e121bd22689f46e500224b99315fc976
Ash <ahoebaer@unamur.be>
2019-10-23 16:28:54 +0200

    orthographe

--- web/signUp.blade.php
+++ web/signUp.blade.php
@@ -3,7 +3,7 @@
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<title>S'inscire | Seed-it</title>
+		<title>S'inscrire | Seed-it</title>
 		<link href="css/signUp.css" rel="stylesheet" type="text/css" media="all" />
 
 	</head>
