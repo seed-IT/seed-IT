27d9e63c35dca4e52a690ee875ab442f5e818384
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2019-11-13 14:22:53 +0100

    Add files via upload

--- API WEB/routes/web.php
+++ API WEB/routes/web.php
@@ -1,7 +1,25 @@
 <?php
 
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/','ViewController@getAccueil');
+
+Route::get('accueil', 'ViewController@getAccueil');
+Route::get('/seed-it', 'ViewController@getUs');
+Route::get('/seedit', 'ViewController@getUs');
 Route::get('contact', 'ContactController@getForm');
 Route::post('contact', 'ContactController@postForm');
-Route::get('racc', 'ControllerAccueil@getAc');
-Route::get('acc', 'ControllerAccueil@getAc');
-Route::get('/','ControllerAccueil@getAc');
+Route::resource('post', 'PostController', ['except' => ['show', 'edit', 'update']]);
+
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
+
