cfa9103cf7427668d9ceb8d775ed1ccb2e1a60d5
Nathan HENRY <44288150+KodiOko@users.noreply.github.com>
2019-12-02 11:58:50 +0100

    2-12-19
    Modif

--- API WEB/resources/views/accueil.blade.php
+++ API WEB/resources/views/accueil.blade.php
@@ -3,22 +3,40 @@
 @section('content')
 
 <div id="article" class="container">
-				<h2>Bienvenue</h2>
-				<p>Le principe même de notre système est de créer une famille de « micro-agriculteurs » et de partager l’envie de cultiver. De nos jours le plus difficile pour commencer une activité est trouver les bonnes inspirations, les bons fournisseurs mais surtout les bons conseils. Ici nous encourageons le partage, et pour le prix d’un petit sachet de graines vous aurez accès illimité à notre réseau de graines non OGM.</p>
-			</div>
+                                <div id="fond">
+                                                <div class="ruban">
+                                                                <h2>Bienvenue</h2>
+                                                </div>
+                                                <div class="ruban_gauche"></div>
+                                                <div class="ruban_droit"></div>
+                                        </div>
+                                <p>Le principe m  me de notre syst  me est de cr  er une famille de    micro-agriculteurs    et de partager l ^`^yenvie de cultiver. De nos jours le plus difficile pour commencer une activit   est trouver les bonnes inspirations, les bons fournisseurs mais surtout les bons conseils. Ici nous encourageons le partage, et pour le prix d ^`^yun petit sachet de graines vous aurez acc  s illimit      notre r  seau de graines non OGM.</p>
+						</div>
 
-			<div id="articleBox" class="tboxContainer">
-				<div class="tbox1">
-					<h2>Les graines</h2>
-					<img src="images/seed.png" alt="Des graines" />
-					<p>Les semences sont la base de toute idée, famille, plante… Afin de pouvoir faciliter la fin de la famine, le partage intra-sociétaire et mettre notre grain dans la lutte contre la pollution.</p>
-				</div>
+                        <div id="articleBox" class="tboxContainer">
+                                <div class="tbox1">
+                                        <div id="fond">
+                                                <div class="ruban">
+                                                                <h2>Les graines</h2>
+                                                </div>
+                                                <div class="ruban_gauche"></div>
+                                                <div class="ruban_droit"></div>
+                                        </div>
+                                        <img src="images/seed.png" alt="Des graines" />
+                                        <p>Les semences sont la base de toute id  e, famille, plante ^`  Afin de pouvoir faciliter la fin de la famine, le partage intra-soci  taire et mettre notre grain dans la lutte contre la pollution.</p>
+								</div>
 
-				<div class="tbox2">
-					<h2>Raspberry Pi</h2>
-					<img src="images/Raspberrypi.png" alt="Raspberry Pi" />
-					<p>Si vous préférez aider votre famille de micro-agriculteurs et montrer la fiabilité de vos graines, nous vous suggérons le plan PiMium. Grâce à celui-ci, vous et les autres internautes pouvez regarder la température et l’humidité de la pièce où se trouvent vos graines. Tout ce que vous avez à faire lors de la réception de l’appareil est de le brancher sur le secteur et au modem internet fourni. Le RaspBerry Pi est une invention magnifique qui permet de partager des informations, en fonction des capteurs connectés dessus.</p>
-				</div>
-			</div>
-		</div>
+                                <div class="tbox2">
+                                        <div id="fond">
+                                                <div class="ruban">
+                                                                <h2>RASPBERRY PI</h2>
+                                                </div>
+                                                <div class="ruban_gauche"></div>
+                                                <div class="ruban_droit"></div>
+                                        </div>
+                                        <img src="images/Raspberrypi.png" alt="Raspberry Pi" />
+                                        <p>Si vous pr  f  rez aider votre famille de micro-agriculteurs et montrer la fiabilit   de vos graines, nous vous sugg  rons le plan PiMium. Gr  ce    celui-ci, vous et les autres internautes pouvez regarder la temp  rature et l ^`^yhumidit   de la pi  ce o   se trouvent vos graines. Tout ce que vous avez    faire lors de la r  ception de l ^`^yappareil est de le brancher sur le secteur et au modem internet fourni. Le RaspBerry Pi est une invention magnifique qui permet de partager des informations, en fonction des capteurs connect  s dessus.</p>
+								</div>
+                        </div>
+                </div>
 @endsection
