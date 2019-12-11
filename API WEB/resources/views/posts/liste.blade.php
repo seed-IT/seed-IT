3d2ca6185ff9bea8e6810a9ae61cb269e801631f
hymedbsk <45210165+hymedbsk@users.noreply.github.com>
2019-12-11 15:41:38 +0100

    Add files via upload

--- API WEB/resources/views/posts/liste.blade.php
+++ API WEB/resources/views/posts/liste.blade.php
@@ -1,15 +1,7 @@
+
 @extends('layouts/templateAll')
 
-@section('header')
-        <!--@if(Auth::check())
-                <div class="btn-group pull-right">
-                        {!! link_to_route('post.create', 'Créer un article', [], ['class' => 'btn btn-info']) !!}
-                        {!! link_to('logout', 'Deconnexion', ['class' => 'btn btn-warning']) !!}
-                </div> -->
-        @else
-                {!! link_to('login', 'Se connecter', ['class' => 'btn btn-info pull-right']) !!}
-        @endif
-@endsection
+
 
 @section('content')
         @if(isset($info))
@@ -25,18 +17,28 @@
                                         <h1>{{ $post->titre }}</h1>
                                 </header>
                                         <hr>
-                                        @if(Auth::check() and Auth::user()->prime)
-                                        <p>{{ $post->contenu }}</p>
+
+                                        <p> Conseils : {{ $post->contenu }}</p>
+
+                                        @if(Auth::check())
+                                        <p> Comment me contacter : {{$post->contact }}</p>
                                         @endif
+                                        <div id="info" >
+                                        <p> Cliquez sur le tag pour filtrer vos recherche --> </p>
+                                        </div>                                                                                                                                                                                                                                        <div id ="tag" class="tag">                                                                                                                                                                                                                 @foreach($post->tags as $tag)
+                                        {!! link_to('post/tag/' . $tag->tag_url, $tag->tag,     ['class' => 'btn btn-xs btn-info']) !!}
+                                                        @endforeach
+                                        </div>
+                                        <em class="pull-right">
+                                                <span class="glyphicon glyphicon-pencil"></span> {{ $post->user->name }} le {!! $post->created_at->format('d-m-Y') !!}
+                                        </em>
                                         @if(Auth::check() and Auth::user()->admin)
                                                 {!! Form::open(['method' => 'DELETE', 'route' => ['post.destroy', $post->id]]) !!}
                                                         {!! Form::submit('Supprimer cet article', ['class' => 'btn btn-danger btn-xs ', 'onclick' => 'return confirm(\'Vraiment supprimer cet article ?\')']) !!}
                                                 {!! Form::close() !!}
                                         @endif
 
-                                        <em class="pull-right">
-                                                <span class="glyphicon glyphicon-pencil"></span> {{ $post->user->name }} le {!! $post->created_at->format('d-m-Y') !!}
-                                        </em>
+
 
                                 </section>
                         </div>
@@ -45,3 +47,5 @@
         @endforeach
         {!! $links !!}
 @endsection
+
+
